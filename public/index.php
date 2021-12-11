8d34c5541f8f38799a5cfe78f18ef88a0bb9d308
Deybic Rojas <deybic9715@gmail.com>
2021-12-11 13:31:04 -0500

    Actualizar ruta style

--- public/index.php
+++ public/index.php
@@ -45,7 +45,6 @@ require __DIR__.'/../vendor/autoload.php';
 */
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
-require_once __DIR__.'/../resources/css/style.css';
 
 $kernel = $app->make(Kernel::class);
 
